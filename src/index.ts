b192a66dfba6034bcac4da8afd19cbb5ad3f29c8
MateusR <mateusftc_@hotmail.com>
2023-10-15 22:06:02 -0300

    feat: Adicionado MongoClient com conexão ao MongoDb (Atlas)

--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,30 @@ import express from "express";
 import { config } from "dotenv";
 import { MongoGetUsersRepository } from "./repositories/get-users/mongo-get-users";
 import { GetUsersControllers } from "./controllers/get-users/get-users";
+import { MongoClient } from "./database/mogo";
 
-config();
+const main = async () => {
+  config();
 
-const app = express();
+  const app = express();
 
-const port = process.env.PORT || 3000;
+  await MongoClient.connect();
 
-app.get("/users", async (req, res) => {
-  const mongoGetUsersRepository = new MongoGetUsersRepository();
+  app.get("/users", async (req, res) => {
+    const mongoGetUsersRepository = new MongoGetUsersRepository();
 
-  const getUsersController = new GetUsersControllers(mongoGetUsersRepository);
+    const getUsersController = new GetUsersControllers(mongoGetUsersRepository);
 
-  const { body, statusCode } = await getUsersController.handle();
+    const { body, statusCode } = await getUsersController.handle();
 
-  res.send(body).status(statusCode);
-});
+    res.send(body).status(statusCode);
+  });
 
-app.listen(port, () => {
-  console.log(`listening on ${port}!`);
-});
+  const port = process.env.PORT || 3000;
+
+  app.listen(port, () => {
+    console.log(`listening on ${port}!`);
+  });
+};
+
+main();
