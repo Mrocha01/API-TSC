4126035d866244a3b8aabc0d8ae1ecf41ed6b767
MateusR <mateusftc_@hotmail.com>
2023-10-19 23:48:33 -0300

    refactor:Adicionado ao controller as helpers functions

--- src/controllers/update-user/update-user.ts
+++ src/controllers/update-user/update-user.ts
@@ -1,4 +1,5 @@
 import { User } from "../../models/user";
+import { badRequest, ok, serverError } from "../helpers";
 import { HttpRequest, HttpResponse, IController } from "../protocols";
 import { IUpdateUserRepository, UpdateUserParams } from "./protocols";
 
@@ -6,51 +7,45 @@ export class UpdateUserController implements IController {
   constructor(private readonly updateUserRepository: IUpdateUserRepository) {}
   async handle(
     httpRequest: HttpRequest<UpdateUserParams>
-  ): Promise<HttpResponse<User>> {
+  ): Promise<HttpResponse<User | string>> {
     try {
       const id = httpRequest?.params?.id;
       const body = httpRequest?.body;
 
       if (!body) {
-        return {
-          statusCode: 400,
-          body: "Missing fields",
-        };
+        return badRequest("Missing fields");
       }
 
       if (!id) {
-        return {
-          statusCode: 400,
-          body: "Missing user id",
-        };
+        return badRequest("Missing user id");
       }
       const allowedFieldsToUpdate: (keyof UpdateUserParams)[] = [
         "firstName",
         "lastName",
         "password",
       ];
+
+      if (
+        allowedFieldsToUpdate.some((field) => String(body[field]).trim() === "")
+      ) {
+        return badRequest(
+          "One or more fields in allowedFieldsToUpdate are empty"
+        );
+      }
+
       const someFieldIsNotAllowedToUpdate = Object.keys(body).some(
         (key) => !allowedFieldsToUpdate.includes(key as keyof UpdateUserParams)
       );
 
       if (someFieldIsNotAllowedToUpdate) {
-        return {
-          statusCode: 400,
-          body: "Some received fields are not allowed",
-        };
+        return badRequest("Some received fields are not allowed");
       }
 
       const user = await this.updateUserRepository.updateUser(id, body);
 
-      return {
-        statusCode: 200,
-        body: user,
-      };
+      return ok<User>(user);
     } catch (error) {
-      return {
-        statusCode: 500,
-        body: "Something went wrong",
-      };
+      return serverError();
     }
   }
 }
